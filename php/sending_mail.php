4f27d202d785b764747f3a2dc27a948fe555c4e0
Sean <kaido0121@gmail.com>
2019-08-04 18:53:02 +0800

    add satellite.html

--- php/sending_mail.php
+++ php/sending_mail.php
@@ -8,7 +8,7 @@ $filename = (isset($_POST['file']) && $_POST['file'] != '') ? $_POST['file'] : N
 $message = $_POST['message'];
 $from = $_POST['email'];
 $subject = $_POST['subject'];
-$mailto = 'kaido0121@gmail.com';
+$mailto = 'kaido0121@email.com';
 
 // check if any attachment
 
